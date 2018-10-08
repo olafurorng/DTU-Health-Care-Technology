408c99246541ac95e84a7d0c4c0890b9293e58f7
Ólafur Örn <olafur@plainvanillagames.com>
2018-10-08 11:35:25 +0200

    Make available to use space bar to shoot

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -21,7 +21,7 @@ public class PlayerController : MonoBehaviour {
 
     private void Update()
     {
-        if (Input.GetButton("Fire1") && Time.time > nextFire) {
+        if ((Input.GetButton("Jump") || Input.GetButton("Fire1")) && Time.time > nextFire) {
             nextFire = Time.time + fireRate;
             //GameObject clone = Instantiate(shot, shotSpawn.position, shotSpawn.rotation) as GameObject;    
             Instantiate(shot, shotSpawn.position, shotSpawn.rotation);    
